1dfe7458f407a258ea064ce2e95b818c5cf05fe9
Leon Li <alii0017@student.monash.edu>
2020-05-14 01:07:07 +0800

    Deleted unused variables, added one function: build_suffix_tree which converts the implicit tree to a suffix tree

--- Ukkonen.py
+++ Ukkonen.py
@@ -41,7 +41,6 @@ class SuffixTree:
     def __init__(self, word):
         self.word = word
         self.rootNode = Node()
-        self.leaf_count = 0
         self.leaf = [None] * len(word)
         self.last_created_node = None  # will be used to assist suffix link assignment
         self.end = GlobalEnd()
@@ -52,82 +51,6 @@ class SuffixTree:
 
     def edge_length(self, node):
         return int(node.end) - node.start + 1
-
-    """def implicit_tree(self):
-        root = self.rootNode
-        word = self.word
-        word_len = len(word)
-        i = 0
-        while i < word_len:
-            j = 0
-            while j <= i:
-                result = self.traverse(i, j, root)
-                if result[2] == 1:
-                    # extension rule 1
-                    result[0].end += 1
-                    j += 1
-                elif result[2] == 2:
-                    # extension rule 2
-                    if result[1] == 0:
-                        new_leaf = Node(j, i, j, result[0])
-                        result[0].children.append(new_leaf)
-                    else:
-                        # in somewhere middle of the path
-                        offset = result[1]
-                        original_node = result[0]
-                        original_start = original_node.start
-                        parent_node = original_node.parent
-                        # change the parent node of the original node to the internal node
-                        internal_node = Node(original_start, offset - 1, -1, parent_node)
-                        new_leaf = Node(j + offset - 1, i, j, internal_node)  # a little bug here, the start index is wrong, need to come up with a new solution
-                        original_node.start = offset
-                        parent_node.children[parent_node.children.index(original_node)] = internal_node
-                        original_node.parent = internal_node
-                        internal_node.children.append(original_node)
-                        internal_node.children.append(new_leaf)
-                    j += 1
-                elif result[2] == 3:
-                    # extension rule 3
-                    break
-            i += 1
-            
-     def traverse(self, i, j, node):
-        current_node = node
-        word = self.word
-        children = node.children
-        index = j
-        offset = 0
-        child_node = None
-        for child in children:
-            if word[child.start] == word[j]:
-                child_node = child
-                break
-        if child_node is None:
-            # the node is not created yet, rule two will be applied
-            return (current_node, 0, 2)
-        else:
-            node_start = child_node.start
-            node_end = child_node.end
-            edge_len = self.edge_length(child_node)
-            if i - j <= edge_len:
-                # when the edge length >= the length of path we need to traverse, we do not skip, else we skip to the
-                # place we want to operate directly.
-                while index <= i and node_start <= node_end and word[index] == word[node_start]:
-                    node_start += 1
-                    index += 1
-                    offset += 1     # denotes the index where an internal node should be created
-                if node_start == node_end + 1:
-                    # rule one will be applied
-                    return (child_node, offset, 1)
-                elif index <= i:
-                    # rule two will be applied
-                    return (child_node, offset, 2)
-                elif node_start <= node_end and index == i + 1:
-                    #rule 3 will be applied
-                    return (child_node, -1, 3)
-            else:
-                return self.traverse(i, j + edge_len, child_node)
-    """
 
     def build_implicit_tree(self):
         word = self.word
@@ -189,6 +112,9 @@ class SuffixTree:
                         self.active_edge = i - self.remaining + 1
             i += 1
 
+    def build_tree(self):
+        self.word += '$'
+        self.build_implicit_tree()
 
     def traverse(self, i):
         """
@@ -232,59 +158,6 @@ class SuffixTree:
                 # rule two
                 return (2, child_node)
 
-
-
-    """def extension_rule_applier(self, i, j):
-        root = self.rootNode
-        children = self.rootNode.children
-        word = self.word
-        node = None
-        for child in children:
-            if word[child.start] == word[j]:
-                node = child
-        if node is None:
-            new_leaf = Node(j, i, j)
-            root.children.append(new_leaf)
-            if self.leaf[j] is not None:
-                print("something wrong with leaf!")
-            self.leaf[j] = new_leaf
-        else:
-            index = j
-            edge_start = node.start
-            already_exist = True
-            while index <= i - 1:
-
-                edge_start += 1
-                index += 1
-
-    def extension_rule_1(self, i, leaf):
-        leaf.end = i
-
-    def extension_rule_2(self, i, j, parent_node):
-        already_exist = False
-        for child in parent_node.children:
-            if child.start == j and child.end == i - 1:
-                already_exist = True
-                parent_node = child
-        if already_exist:
-            leaf_node = Node(j, i, j)
-            parent_node.children.append(leaf_node)
-        else:
-            internal_node = Node(j, i - 1)
-            internal_node.children.append(parent_node.children[j])
-            parent_node.children[j] = internal_node
-            # the above step is wrong as the parent node is not always the root. Fix this later 
-            leaf_node = Node(j, i, j)
-            internal_node.children.append(leaf_node)
-
-    def find_node(self, i, j, word, node):
-        for child in node.children:
-            if word[child.start] == word[j]:
-                pass
-            else:
-                return False
-        return False
-    """
     def search(self, query_word):
         root = self.rootNode
         for char in query_word:
@@ -304,39 +177,9 @@ class SuffixTree:
     def __repr__(self):
         return str(self.rootNode)
 
-    """
-    def trace_end(self, node: Node):
-        max_occ = node.count_end()
-        max_occ_word = node
-        for child in node.children:
-            if not child.end:
-                child_trace_result = self.trace_end(child)
-                if child_trace_result[0] > max_occ:
-                    max_occ = child_trace_result[0]
-                    max_occ_word = child_trace_result[1]
-        return (max_occ, max_occ_word)
-
-    def most_common_word(self, query_prefix):
-        root = self.rootNode
-        for char in query_prefix:
-            char_not_found = True
-            for child in root.children:
-                if char == child.value:
-                    root = child
-                    char_not_found = False
-            if char_not_found:
-                return "Not found"
-        result_word = self.trace_end(root)[1]
-        result =[]
-        while result_word.mother_node is not None:
-            result.append(result_word.value)
-            result_word = result_word.mother_node
-        result.reverse()
-        result = "".join(result)
-        return result"""
 
 
-s = SuffixTree("mississippi")
+s = SuffixTree("oooo$")
 s.build_implicit_tree()
 print(s.rootNode)
 
